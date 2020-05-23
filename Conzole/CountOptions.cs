5cafcccb9648d691b7f52978c6fafdb3008eebe1
Eric Cooper <e.cooper92@gmail.com>
2020-05-23 10:43:41 -0500

    Added PostNewLine to count options

--- Conzole/CountOptions.cs
+++ Conzole/CountOptions.cs
@@ -11,5 +11,10 @@ namespace Conzole
         /// Formats the result of count. Default "<count> result(s)"
         /// </summary>
         public Func<int, string> ResultFormatter { get; set; } = count => $"{count} result(s)";
+
+        /// <summary>
+        /// If true, a newline will be added after the list has finished printing. Default true.
+        /// </summary>
+        public bool PostNewLine { get; set; } = true;
     }
 }
